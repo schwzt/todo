bea827fb21d7df121d0f11f9b1c30e701107edcc
schwzt <schwzt@yandex.ru>
2021-06-18 16:03:54 +0300

    Boiler for todo

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(`${__dirname}/date.js`)
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "Cook Food"];
+const workItems = [];
+
+app.get("/", (req, res) => {
+  let day = date.getDate()
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(3000, () => {
+  console.log("Server has been started on port 3000");
+});
